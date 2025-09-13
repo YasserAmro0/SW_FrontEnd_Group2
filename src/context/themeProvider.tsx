56d335b102d83453ad9ca0fce1c1eb9baac7a39d
Ahmed-Alabadla <eng.ahmedalabadla@gmail.com>
2025-09-13 12:20:38 +0300

    feat: implement authentication context and admin login page with validation

---
+++ src/context/themeProvider.tsx
@@ -0,0 +1,28 @@
+import React, { useCallback, useMemo } from 'react';
+import { ThemeProvider } from '@mui/material';
+import { AppContextProps } from './types';
+import { theme, darkTheme } from '../theme/theme';
+import ThemeContext from './themeContext';
+
+const CheckThemeProvider : React.FC<AppContextProps> = ({ children }) => {
+  const [themeMode, setThemeMode] = React.useState<'light' | 'dark'>('light');
+
+  const handleThemeModeSwitch = useCallback(() => {
+    setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  }, []);
+
+  const ThemeContextValue = useMemo(
+    () => ({ themeMode, handleThemeModeSwitch }),
+    [themeMode, handleThemeModeSwitch],
+  );
+
+  return (
+    <ThemeContext.Provider value={ThemeContextValue}>
+      <ThemeProvider theme={themeMode === 'light' ? theme : darkTheme}>
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export default CheckThemeProvider;
